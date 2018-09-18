e98f99ab69262c300b08fd1346bc5f6ad9c78bfc
Zer1Ic3 <dwillemse28@gmail.com>
2018-09-18 21:16:22 +0200

    Fix wifi.py

--- wifi.py
+++ wifi.py
@@ -1,22 +1,6 @@
 import os
 import subprocess
 
-'''
-Clone below application and verify require aplication
-git clone https://github.com/hashcat/hashcat.git
-git clone https://github.com/ZerBea/hcxdumptool.git
-git clone https://github.com/ZerBea/hcxtools.git
-Install below
-1: apt-get update
-2: apt-get dist-upgrade -y
-3: apt-get install libssl-dev
-4: apt-get install libz-dev
-5: apt-get install libpcap-dev
-6: apt-get install libcurl4-openssl-dev
-'''
-
-
-
 interface="wlan0mon"
 path="/Pentesting/Wifi_tools/PMKID/bssid/bssid.txt"
 
@@ -52,7 +36,7 @@ subprocess.call(["hcxpcaptool", "-z", "hash2", "hash"])
 os.system('mv hash2 /Pentesting/Wifi_tools/PMKID/hashcat')
 os.chdir('/Pentesting/Wifi_tools/PMKID/hashcat')
 wordlst = raw_input('Please enter name of the wordlist that you want use, wordlst1.txt or wordlst2.lst or wordlst3.lst: \n')
-os.system("xterm -e './hashcat -m 16800 hash2 /root/wordlists/wordlst --force; read'")
+os.system("xterm -e './hashcat -m 16800 hash2 wordlst --force; read'")
 
 wait = raw_input('Please wait for decrypting to finish, Then hit enter key: \n')
 subprocess.call(["./hashcat", "-m", "16800", "hash2", "--force", "--show"])
